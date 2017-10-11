5473125e8867639ed531bf079d5c6bb750c93a00
George <beckg2@seattleu.edu>
2017-10-11 00:17:26 -0700

    new
    new

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# beckg2.github.io
-George's Personal code portfolio.
+# beckg2githubio
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.16.0.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name beckg2githubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the beckg2githubioApp
+ */
+angular.module('beckg2githubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
