dbf1b53048e6b9f6a82e9f8fc1b96e5048596771
George <beckg2@seattleu.edu>
2017-11-13 17:20:02 -0800

    rebuild

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name beckg2githubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the beckg2githubioApp
+ */
+angular.module('beckg2githubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
